4a777fdaba28275fd4d4c216a7ac34159909b69a
imprisoner <continentalresident@yandex.ru>
2023-08-01 03:05:54 +0400

    remove console.log

--- src/hooks/messages/is-sockets.js
+++ src/hooks/messages/is-sockets.js
@@ -2,7 +2,7 @@ import { Unprocessable } from '@feathersjs/errors'
 
 export const isSockets = (context, next) => {
   const { params: { provider } } = context
-  console.log(provider)
+
   if (provider !== 'socketio') {
     throw new Unprocessable('Not allowed creating messages outside chat rooms')
   }
