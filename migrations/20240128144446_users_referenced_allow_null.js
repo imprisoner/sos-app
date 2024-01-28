2ca1532e454e9a42b17ba6cb0a8058b81b320d0c
arsgal <continentalresident@yandex.ru>
2024-01-28 20:14:14 +0400

    added migration removing NOT NULL constraints from foreign keys referenced "users" table

---
+++ migrations/20240128144446_users_referenced_allow_null.js
@@ -0,0 +1,69 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function up(knex) {
+  await knex.raw(`
+    ALTER TABLE "account-providers"
+    DROP CONSTRAINT account_providers_userid_foreign;
+
+    ALTER TABLE "device-tokens"
+    DROP CONSTRAINT device_tokens_userid_foreign;
+
+    ALTER TABLE "messages"
+    DROP CONSTRAINT messages_userid_foreign;
+
+    ALTER TABLE "notifications"
+    DROP CONSTRAINT notifications_recipientid_foreign;
+
+    ALTER TABLE "rooms"
+    DROP CONSTRAINT rooms_patient_foreign;
+    
+    ALTER TABLE "rooms"
+    DROP CONSTRAINT rooms_volunteer_foreign;
+  `)
+
+  await knex.schema.alterTable('rooms', allowSetNullOnDelete(['patient', 'volunteer']))
+  await knex.schema.alterTable('account-providers', allowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('device-tokens', allowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('messages', allowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('notifications', allowSetNullOnDelete(['recipientId']))
+}
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+export async function down(knex) {
+  await knex.schema.alterTable('rooms', undoAllowSetNullOnDelete(['patient', 'volunteer']))
+  await knex.schema.alterTable('account-providers', undoAllowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('device-tokens', undoAllowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('messages', undoAllowSetNullOnDelete(['userId']))
+  await knex.schema.alterTable('notifications', undoAllowSetNullOnDelete(['recipientId']))
+}
+
+
+function allowSetNullOnDelete(columns = []) {
+  return (table) => {
+    columns.forEach((col) => {
+      table.uuid(col)
+        .alter({ alterType: false })
+        .references('id')
+        .inTable('users')
+        .nullable()
+        .onDelete('SET NULL')
+    })
+  }
+}
+
+function undoAllowSetNullOnDelete(columns = []) {
+  return (table) => {
+    columns.forEach((col) => {
+      table.uuid(col)
+        .alter({ alterType: false })
+        .references('id')
+        .inTable('users')
+        .notNullable()
+        .onDelete('NO ACTION')
+    })
+  }
+}
